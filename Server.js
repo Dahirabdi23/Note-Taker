0744a0a4f190d49d80ff6d99381d6c105fc6d92d
Dahirabdi23 <86687537+Dahirabdi23@users.noreply.github.com>
2022-01-15 09:45:58 -0500

    deploy

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const { dirname } = require('path');
 const path = require('path');
 const api = require('./routes/apiRoutes');
 
@@ -14,11 +15,11 @@ app.use(express.static('public'));
 app.use('/', api);
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.listen(PORT, () =>
